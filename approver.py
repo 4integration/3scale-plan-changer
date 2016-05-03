8ca4b3d36b6599bb2e8b08551b322cbf393ea7bd
Sam Weston <admin@samuelweston.com>
2016-05-03 17:07:42 +0100

    Docstrings are good!

--- approver.py
+++ approver.py
@@ -14,16 +14,34 @@ import argparse
 
 
 def get_account_xml(provider_key, api_endpoint):
+    """Downloads the list of customer accounts from 3Scale for our provider key and api endpoint and returns the XML
+    as a string. May return None if we get an error code from 3Scale.
+
+    :param str provider_key: The 3Scale Provider Key
+    :param str api_endpoint: The 3Scale API Endpoint
+
+    :return: Customer accounts XML response
+    :rtype str or None
+    """
+
     r = requests.get('https://' + api_endpoint + '/admin/api/accounts.xml?provider_key=' + provider_key +
                      '&state=approved')
     if r.status_code != 200:
-        print("Error " + r.status_code + " getting account xml from 3scale")
+        print("Error: Code " + r.status_code + " while getting account xml from 3scale")
         return None
 
     return r.text
 
 
 def get_accounts_with_card(account_xml):
+    """Processes the 3Scale list of customer accounts and returns the id for any that have credit cards stored.
+
+    :param str account_xml: The 3Scale Response XML with a list of customer accounts.
+
+    :return: List of 3Scale account ids.
+    :rtype: list[str] or None
+    """
+
     root = etree.fromstring(account_xml.encode('utf-8'))
     account_objects = root.findall("account")
     account_list = []
@@ -38,17 +56,36 @@ def get_accounts_with_card(account_xml):
 
 
 def get_application_xml(account_id, provider_key, api_endpoint):
+    """Downloads the list of applications from 3Scale for a given account_id and returns the XML as a string. May return
+     None if we get an error code from 3Scale.
+
+    :param str account_id: The 3Scale Customer Account ID.
+    :param str provider_key: The 3Scale Provider Key
+    :param str api_endpoint: The 3Scale API Endpoint
+
+    :return: Applications XML response
+    :rtype str or None
+    """
+
     r = requests.get('https://' + api_endpoint + '/admin/api/accounts/' + account_id +
                      '/applications.xml?provider_key=' + provider_key)
 
     if r.status_code != 200:
-        print("Error" + r.status_code + " getting application xml for account " + account_id)
+        print("Error: Code " + r.status_code + " while getting application xml for account " + account_id)
         return None
 
     return r.text
 
 
 def check_application_status(application_xml):
+    """Processes the applications xml and returns the id for any that are pending.
+
+    :param str application_xml: The 3Scale Response XML with a list of applications for a account.
+
+    :return: List of applications that are Pending
+    :rtype: list[str] or None
+    """
+
     root = etree.fromstring(application_xml.encode('utf-8'))
     application_objects = root.findall("application")
     application_list = []
@@ -66,6 +103,17 @@ def check_application_status(application_xml):
 
 
 def enable_application(account_id, application_id, provider_key, api_endpoint):
+    """Sends a request to 3scale to accept an application.
+
+    :param str account_id: The 3Scale Customer Account ID.
+    :param str application_id: The 3Scale Application ID.
+    :param str provider_key: The 3Scale Provider Key
+    :param str api_endpoint: The 3Scale API Endpoint
+
+    :return: None
+    :rtype: NoneType
+    """
+
     print("Accepting application " + application_id + " for account " + account_id)
     r = requests.put('https://' + api_endpoint + '/admin/api/accounts/' + account_id + '/applications/' +
                      application_id + '/accept.xml', data={'provider_key': provider_key})
@@ -73,7 +121,7 @@ def enable_application(account_id, application_id, provider_key, api_endpoint):
     if r.status_code == 200:
         print("Accepting application worked for " + application_id + " for account " + account_id)
     else:
-        print("Error " + r.status_code + " accepting application for " + application_id + " for account " + account_id)
+        print("Error: Code " + r.status_code + " while accepting application for " + application_id + " for account " + account_id)
 
 
 if __name__ == '__main__':
@@ -91,9 +139,13 @@ if __name__ == '__main__':
         exit(1)
 
     accounts = get_accounts_with_card(get_account_xml(threescale_provider_key, threescale_api_endpoint))
-    for account in accounts:
-        applications = check_application_status(get_application_xml(account, threescale_provider_key,
-                                                                    threescale_api_endpoint))
-        if applications:
-            for application in applications:
-                enable_application(account, application, threescale_provider_key, threescale_api_endpoint)
+    if not isinstance(accounts, list):
+        print("Warning: No accounts found with card details stored")
+        exit(0)
+    else:
+        for account in accounts:
+            applications = check_application_status(get_application_xml(account, threescale_provider_key,
+                                                                        threescale_api_endpoint))
+            if applications:
+                for application in applications:
+                    enable_application(account, application, threescale_provider_key, threescale_api_endpoint)
